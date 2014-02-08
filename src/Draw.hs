b8c19aee77d04da90fc2ee8f46b2596cf289da79
TrevorBender <Trevor.Bender@gmail.com>
2014-02-08 08:30:32 -0500

    Add hud

--- src/Draw.hs
+++ src/Draw.hs
@@ -8,51 +8,60 @@ import System.Console.ANSI
 
 import Game
 
+drawGame :: Game -> IO ()
+drawGame game = drawScreen (ui game) game
+
 drawBlock :: Int -> Int -> [String] -> IO ()
 drawBlock y x = (mapM_ $ \(i, s) -> do
     setCursorPosition (y + i) x
     putStr s
     ) . zip [0..]
 
+drawStr :: Int -> Int -> String -> IO ()
+drawStr y x str = setCursorPosition y x >> putStr str
+
 drawScreen :: Screen -> Game -> IO ()
 drawScreen Start _ = do
-    setCursorPosition 4 4
-    putStr "Welcome to the Caves of Slight Danger"
+    drawStr 4 4 "Welcome to the Caves of Slight Danger"
     drawBlock 15 4 [ "Press [enter] to Start Playing"
                    , "Press [q] to Quit" ]
 
 drawScreen Win _ = do
-    setCursorPosition 4 4
-    putStr "You WIN!"
+    drawStr 4 4 "You WIN!"
     drawBlock 15 4 [ "Press [esc] to Quit"
                    , "Press <Anything> to Go back to Start" ]
 
 drawScreen Lose _ = do
-    setCursorPosition 4 4
-    putStr "You LOSE!"
+    drawStr 4 4 "You LOSE!"
     drawBlock 15 4 [ "Press [esc] to Quit"
                    , "Press <Anything> to Go back to Start" ]
 
 drawScreen Play game = do
-    setCursorPosition 0 0
     drawLevel game
+    drawPlayer game
+    drawCreatures game
+    drawHud game
+
+drawHud :: Game -> IO ()
+drawHud game = do
+    let p = game^.player
+    resetColor
+    drawStr gameHeight 0 $ "loc=" ++ show (p^.location) ++ " hp=[" ++ show (p^.hp) ++ "/" ++ show (p^.maxHp) ++ "]"
+
+drawCreatures :: Game -> IO ()
+drawCreatures game = forM_ (M.elems $ M.filter sameDepth $ game^.creatures) drawCreature
+    where sameDepth c = depth == playerDepth
+            where (_,_,depth) = c^.location
+                  (_,_,playerDepth) = game^.player^.location
 
 drawLevel :: Game -> IO ()
 drawLevel game = do
     drawBlock 0 0 lvl
-    drawPlayer game
-    forM_ (M.elems $ M.filter sameDepth $ game^.creatures) drawCreature
     where (_,_,depth) = game^.player.location
           lvl2d = (splitBy (gameWidth * gameHeight) $ A.elems $ game^.level) !! depth
           rows = splitBy gameWidth lvl2d
           lvl = map row2str rows
           row2str = foldr (\tile str -> (tile^.glyph) : str) ""
-          sameDepth c = depth == playerDepth
-            where (_,_,depth) = c^.location
-                  (_,_,playerDepth) = game^.player^.location
-
-drawGame :: Game -> IO ()
-drawGame game = drawScreen (ui game) game
 
 resetColor :: IO ()
 resetColor = setSGR [ SetColor Foreground Vivid Black ]
